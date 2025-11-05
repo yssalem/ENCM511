a44500907b355fe254c825e8f5524e12ab62a0e8
noor055 <160282767+noor055@users.noreply.github.com>
2025-11-05 14:13:00 -0700

    Add timer.h header file with timer functions

---
+++ lab4/timer.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   timer.h
+ * Author: youss
+ *
+ * Created on October 24, 2025, 7:56 PM
+ */
+
+#ifndef TIMER_H
+#define	TIMER_H
+
+#include <xc.h>
+
+void TM2init(void);
+void TM3init(void);
+void delay_ms(uint16_t ms);
+void debounce(uint16_t ms);
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TIMER_H */
