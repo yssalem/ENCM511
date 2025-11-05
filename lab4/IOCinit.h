d0d1594b2ae2b8f2c9fca9bedbb9fa1c38577d1e
noor055 <160282767+noor055@users.noreply.github.com>
2025-11-05 13:52:00 -0700

    Create IOCinit.h

---
+++ lab4/IOCinit.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   IOCinit.h
+ * Author: youss
+ *
+ * Created on October 24, 2025, 5:54 PM
+ */
+
+#ifndef IOCINIT_H
+#define	IOCINIT_H
+
+#include <xc.h>
+
+void IOCinit(void);
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* IOCINIT_H */
