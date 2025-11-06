c218db937c8125ccdfaf3edcb8fbc4a3bd82a502
Youssef Salem <156625440+yssalem@users.noreply.github.com>
2025-11-05 18:55:17 -0700

    Update mainAssign4.c

--- lab4/mainAssign4.c
+++ lab4/mainAssign4.c
@@ -1,63 +1,3 @@
-/*
- * File:   main.c
- * Author: Ibrahim Khalid, Noor Sheikh, Youssef Salem
- *
- * Created FOR ENCM 511
- * PLEASE ADD DATE CREATED HERE: 2025-10-14
- * 
- * FAILURE TO UPDATE THIS HEADER WITH YOUR GROUP MEMBER NAMES
- * MAY RESULT IN PENALTIES
- */
-
-// FSEC
-#pragma config BWRP = OFF    //Boot Segment Write-Protect bit->Boot Segment may be written
-#pragma config BSS = DISABLED    //Boot Segment Code-Protect Level bits->No Protection (other than BWRP)
-#pragma config BSEN = OFF    //Boot Segment Control bit->No Boot Segment
-#pragma config GWRP = OFF    //General Segment Write-Protect bit->General Segment may be written
-#pragma config GSS = DISABLED    //General Segment Code-Protect Level bits->No Protection (other than GWRP)
-#pragma config CWRP = OFF    //Configuration Segment Write-Protect bit->Configuration Segment may be written
-#pragma config CSS = DISABLED    //Configuration Segment Code-Protect Level bits->No Protection (other than CWRP)
-#pragma config AIVTDIS = OFF    //Alternate Interrupt Vector Table bit->Disabled AIVT
-
-// FBSLIM
-#pragma config BSLIM = 8191    //Boot Segment Flash Page Address Limit bits->8191
-
-// FOSCSEL
-#pragma config FNOSC = FRC    //Oscillator Source Selection->Internal Fast RC (FRC)
-#pragma config PLLMODE = PLL96DIV2    //PLL Mode Selection->96 MHz PLL. Oscillator input is divided by 2 (8 MHz input)
-#pragma config IESO = OFF    //Two-speed Oscillator Start-up Enable bit->Start up with user-selected oscillator source
-
-// FOSC
-#pragma config POSCMD = NONE    //Primary Oscillator Mode Select bits->Primary Oscillator disabled
-#pragma config OSCIOFCN = ON    //OSC2 Pin Function bit->OSC2 is general purpose digital I/O pin
-#pragma config SOSCSEL = OFF    //SOSC Power Selection Configuration bits->Digital (SCLKI) mode
-#pragma config PLLSS = PLL_FRC    //PLL Secondary Selection Configuration bit->PLL is fed by the on-chip Fast RC (FRC) oscillator
-#pragma config IOL1WAY = ON    //Peripheral pin select configuration bit->Allow only one reconfiguration
-#pragma config FCKSM = CSECMD    //Clock Switching Mode bits->Clock switching is enabled,Fail-safe Clock Monitor is disabled
-
-// FWDT
-#pragma config WDTPS = PS32768    //Watchdog Timer Postscaler bits->1:32768
-#pragma config FWPSA = PR128    //Watchdog Timer Prescaler bit->1:128
-#pragma config FWDTEN = ON_SWDTEN    //Watchdog Timer Enable bits->WDT Enabled/Disabled (controlled using SWDTEN bit)
-#pragma config WINDIS = OFF    //Watchdog Timer Window Enable bit->Watchdog Timer in Non-Window mode
-#pragma config WDTWIN = WIN25    //Watchdog Timer Window Select bits->WDT Window is 25% of WDT period
-#pragma config WDTCMX = WDTCLK    //WDT MUX Source Select bits->WDT clock source is determined by the WDTCLK Configuration bits
-#pragma config WDTCLK = LPRC    //WDT Clock Source Select bits->WDT uses LPRC
-
-// FPOR
-#pragma config BOREN = ON    //Brown Out Enable bit->Brown Out Enable Bit  
-#pragma config LPCFG = OFF    //Low power regulator control->No Retention Sleep
-#pragma config DNVPEN = ENABLE    //Downside Voltage Protection Enable bit->Downside protection enabled using ZPBOR when BOR is inactive
-
-// FICD
-#pragma config ICS = PGD1    //ICD Communication Channel Select bits->Communicate on PGEC1 and PGED1
-#pragma config JTAGEN = OFF    //JTAG Enable bit->JTAG is disabled
-
-// FDEVOPT1
-#pragma config ALTCMPI = DISABLE    //Alternate Comparator Input Enable bit->C1INC, C2INC, and C3INC are on their standard pin locations
-#pragma config TMPRPIN = OFF    //Tamper Pin Enable bit->TMPRN pin function is disabled
-#pragma config SOSCHP = ON    //SOSC High Power Enable bit (valid only when SOSCSEL = 1->Enable SOSC high power mode (default)
-#pragma config ALTI2C1 = ALTI2CEN    //Alternate I2C pin Location->SDA1 and SCL1 on RB9 and RB8
 
 #include <xc.h>
 #include "uart.h"
@@ -66,29 +6,54 @@
 #include "IOCinit.h"
 #include "timer.h"
 
-#define LED0 LATBbits.LATB5
-#define LED1 LATBbits.LATB6
-#define LED2 LATBbits.LATB7
-#define PB0 PORTBbits.RB8
-
-uint16_t do_ADC(void);
 uint16_t conv = 0;
 long width = 0;
-char bar_character = 'x';
+volatile char bar_character = 'x';
+
+typedef enum {HEX, DEC} state_t;
+state_t state = DEC;
+
 
 
 int main() {
     
     TM2init();
     IOinit();
+    IOCinit();
     ADCinit();
     InitUART2();
     
     while(1) {
+        
+        if (RXFlag) {
+            switch (received_char) {
+                case 'x':
+                    state = HEX;
+                    break;
+                case 'd':
+                    state = DEC;
+                    break;
+                default:
+                    break;
+            }
+            
+            RXFlag = 0;
+        }
+        
         ClearTerminal();
         conv = do_ADC();
         width = ((float)conv / 1024) * 48;
         XmitUART2(bar_character, width);
+        Disp2String(" ");
+        
+        switch (state) {
+            case HEX:
+                Disp2Hex(conv);
+                break;
+            case DEC:
+                Disp2Dec(conv);
+                break;
+        }
         delay_ms(1000);
         
     }
@@ -101,5 +66,25 @@ void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void){
     T2CONbits.TON = 0; // turn timer OFF
 }
 
+void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void){
+    //Don't forget to clear the timer 3 interrupt flag!
+
+    IFS0bits.T3IF = 0; // reset flag
+    T3CONbits.TON = 0; // turn timer OFF
+}
+
+void __attribute__ ((interrupt, no_auto_psv)) _IOCInterrupt(void) {
+
+    
+    for (uint16_t i = 0; i < 20000; i++);
+    
+    if ((!PB0 && !PB1) || (!PB0 && !PB2) || (!PB1 && !PB2));
+    else if (!PB0) bar_character = 'x';
+    else if (!PB1) bar_character = '-';
+    else if (!PB2) bar_character = '=';
+    
+    IFS1bits.IOCIF = 0;
+    
 
 
+}
